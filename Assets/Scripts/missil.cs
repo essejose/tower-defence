5205192a67d67f8d2b9276971506c5aba14b180f
essejose <ze_pk2@hotmail.com>
2016-10-20 00:25:00 -0200

    create cannon tower

---
+++ Assets/Scripts/missil.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class missil : MonoBehaviour {
+
+    private float velocidade = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Anda();
+	}
+
+    private void Anda()
+    {
+        Vector3 posicaoAtual = transform.position;
+        Vector3 deslocamento = transform.forward * Time.deltaTime * velocidade;
+        transform.position = posicaoAtual + deslocamento;
+    }
+}
---
+++ Assets/Scripts/torre.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class torre : MonoBehaviour {
+
+    public GameObject projeilPrefab;
+    public float tempoDeRecarga = 1f;
+    private float momentoDoUltimoDisparo;
+    // Use this for initialization
+	void Update () {
+        Atira();
+    }
+	
+	 public void Atira()
+    {
+        float tempoAtual = Time.time;
+
+        if(tempoAtual > momentoDoUltimoDisparo + tempoDeRecarga)
+        {
+            momentoDoUltimoDisparo = tempoAtual;
+        GameObject pontoDeDisparo = this.transform.Find("CanhaoDaTorre/PontoDeDisparo").gameObject;
+        Vector3 posicaoDoPontoDeDisparo = pontoDeDisparo.transform.position;
+        Instantiate(projeilPrefab,posicaoDoPontoDeDisparo,Quaternion.identity);
+        }
+    }
+}
