720fc971c9d193f2f81282b865ed9c1c205e9ee9
essejose <ze_pk2@hotmail.com>
2016-10-19 23:18:53 -0200

    struture

---
+++ Assets/Scripts/inimigo.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class inimigo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        NavMeshAgent agente = GetComponent<NavMeshAgent>();
+        GameObject fimdocaminho = GameObject.Find("FimDoCaminho");
+        Vector3 posicaoDofimCaminho = fimdocaminho.transform.position;
+        agente.SetDestination(posicaoDofimCaminho);
+
+	}
+ 
+}
