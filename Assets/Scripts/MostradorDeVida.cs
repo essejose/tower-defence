85e36057ef7d75b54b5e48878e0d37fdd6418982
essejose <ze_pk2@hotmail.com>
2016-11-03 23:10:46 -0200

    life indicador

--- Assets/Scripts/MostradorDeVida.cs
+++ Assets/Scripts/MostradorDeVida.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MostradorDeVida : MonoBehaviour {
 
+    private Text campoTexto;
+    [SerializeField]
+    private Jogador jogador;
 	// Use this for initialization
 	void Start () {
-	
+
+       campoTexto = GetComponent<Text>();
 	}
+
+    void Update()
+    {
+        campoTexto.text = "Vida :" + jogador.GetVida() ;
+    }
 	
-	// Update is called once per frame
-	void Update () {
 	
-	}
 }
